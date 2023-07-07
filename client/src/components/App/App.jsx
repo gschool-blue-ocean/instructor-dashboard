7a72a53bf705adc4749bf224fe2c0dafe8312c88
timgall <119544130+timgall@users.noreply.github.com>
2023-07-07 17:01:39 -0500

    updated header

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -4,14 +4,15 @@ import { Route, Routes } from "react-router-dom";
 import { AuthContextProvider } from "../../context/authContext";
 import LogIn from "../LogIn/LogIn";
 import SignUpForm from "../Sign-up Page/SignUpForm";
-
+import Header from "../Header/Header";
 const App = () => {
   return (
     <div>
       <h1 className="text-center text 3xl font-bold">Firebase auth</h1>
       <AuthContextProvider>
         <Routes>
-          <Route path="/" element={<LogIn />} />
+          <Route path="/" element={<Header />} />
+          {/* <Route path="/" element={<LogIn />} /> */}
           <Route path="/signup" element={<SignUpForm />} />
         </Routes>
       </AuthContextProvider>
