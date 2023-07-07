968b62e5d26bf9580243bdc0228678db36fd133c
JosephCarmeli <josephcarmeli@hotmail.com>
2023-07-07 10:35:18 -0500

    added firebase auth

---
+++ client/Firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBh7mYyirL-e4-pV7S7rnZjZIifZEAZgJc",
+  authDomain: "instructor-dashboard-4734f.firebaseapp.com",
+  projectId: "instructor-dashboard-4734f",
+  storageBucket: "instructor-dashboard-4734f.appspot.com",
+  messagingSenderId: "530969494205",
+  appId: "1:530969494205:web:f547204f30fd90ab8b6dd4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,8 +1,24 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Route, Routes } from "react-router-dom";
+import Signup from "../signin/login/Signup";
+import Login from "../signin/login/Login";
+import Account from "../signin/login/Account";
+import { AuthContextProvider } from "../../context/authContext";
 
 const App = () => {
-  return <main>yolo</main>;
+  return (
+    <div>
+      <h1 className="text-center text 3xl font-bold">Firebase auth</h1>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
+    </div>
+  );
 };
 
 export default App;
