620bbcce943194f6b827f6abf8b07ddb8a3f2d0f
John Kluse <j.kluse@gmail.com>
2023-07-12 11:34:55 -0600

    fixed justify content and css

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,28 +1,27 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Menu, MenuItem } from "react-pro-sidebar";
 import { AiFillHome } from "react-icons/ai";
 import { BsPersonFillAdd } from "react-icons/bs";
 
-const App = () => {
-  return (
-    <div className=" w-36 pt-12 ">
-      <Sidebar>
-        <Menu className=" w-36 h-screen text-white bg-zinc-600">
-          <div>
-            <div className="flex items-center py-1 text-white bg-zinc-600 hover:text-opacity-60 md:hover:border md:hover:border-teal-800 md:hover:bg-teal-800">
-              <AiFillHome className="w-7 h-7 mr-2" />
-              <a href="www.yahoo.com">Home</a>
-            </div>
-          </div>
-          <div>
-            <div className="flex items-center py-1 text-white bg-zinc-600 hover:text-opacity-60 md:hover:border md:hover:border-teal-800 md:hover:bg-teal-800">
-              <BsPersonFillAdd className="w-7 h-7 mr-2" />
-              <a>Add User</a>
-            </div>
-          </div>
-        </Menu>
-      </Sidebar>
-    </div>
-  );
+//got rid of sidebar component
+const Sidebar = () => {
+	return (
+		<div className=" w-36 pt-4 bg-zinc-600">
+			<Menu className=" h-screen text-white bg-zinc-600">
+				<div>
+					<div className="flex items-center p-2 text-white bg-zinc-600 hover:text-opacity-60 md:hover:border md:hover:border-teal-800 md:hover:bg-teal-800">
+						<AiFillHome className="w-7 h-7 mr-2" />
+						<a href="www.yahoo.com">Home</a>
+					</div>
+				</div>
+				<div>
+					<div className="flex items-center p-2 text-white bg-zinc-600 hover:text-opacity-60 md:hover:border md:hover:border-teal-800 md:hover:bg-teal-800">
+						<BsPersonFillAdd className="w-7 h-7 mr-2" />
+						<a>Add User</a>
+					</div>
+				</div>
+			</Menu>
+		</div>
+	);
 };
-export default App;
+export default Sidebar;
