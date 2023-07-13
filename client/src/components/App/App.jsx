b6653a431e97cdb88337d390468d362e7058abc7
galVDalton <D191Andrews@gmail.com>
2023-07-13 11:22:42 -0500

    Update to Components

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -6,6 +6,10 @@ import LogIn from "../LogIn/LogIn";
 import SignUpForm from "../Sign-up Page/SignUpForm";
 import StudentOverview from "../Student-overview/StudentSummary";
 import Header from "../Header/Header";
+import AssignmentDetails from "../Assignment/Assignments";
+import ProjectDetails from "../Projects/Projects";
+import Feedback from "../Projects/Feedback";
+import AssessDetails from "../Assessments/Assessments";
 
 const App = () => {
   return (
@@ -17,6 +21,10 @@ const App = () => {
           {/* <Route path="/" element={<LogIn />} /> */}
           <Route path="/signup" element={<SignUpForm />} />
           <Route path="/studentoverview" element={<StudentOverview />} />
+          <Route path="/student_projects" element={<ProjectDetails />} />
+          <Route path="/student_assignments" element={<AssignmentDetails />} />
+          <Route path="/project_feedback" element={<Feedback />} />
+          <Route path="/student_assessment" element={<AssessDetails />} />
         </Routes>
       </AuthContextProvider>
     </div>
