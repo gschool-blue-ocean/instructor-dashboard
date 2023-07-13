a1e10c890f8c71d8b9e876cff2ab0fdddcba3c13
John Kluse <j.kluse@gmail.com>
2023-07-13 12:23:56 -0600

    Update App.jsx
    Updated App.jsx file from current main branch

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,22 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { Route, Routes } from 'react-router-dom'
-import { AuthContextProvider } from '../../context/authContext'
-import LogIn from '../LogIn/LogIn'
-import SignUpForm from '../Sign-up Page/SignUpForm'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Route, Routes } from "react-router-dom";
+import { AuthContextProvider } from "../../context/authContext";
+import LogIn from "../LogIn/LogIn";
+import SignUpForm from "../Sign-up Page/SignUpForm";
+import StudentOverview from "../Student-overview/StudentSummary";
+import Header from "../Header/Header";
+import AssignmentDetails from "../Assignment/Assignments";
+import ProjectDetails from "../Projects/Projects";
+import Feedback from "../Projects/Feedback";
+import AssessDetails from "../Assessments/Assessments";
+import StudentCard from "../StudentCard";
 
 const App = () => {
-    return (
-        <div>
-            <h1 className="text-center text 3xl font-bold">Firebase auth</h1>
-            <AuthContextProvider>
-                <Routes>
-                    <Route path="/" element={<LogIn />} />
-                    <Route path="/signup" element={<SignUpForm />} />
-                </Routes>
-            </AuthContextProvider>
-        </div>
-    )
-}
+	return (
+		<div>
+			<h1 className="text-center text 3xl font-bold"></h1>
+			<AuthContextProvider>
+				<Routes>
+					<Route path="/" element={<Header />} />
+					{/* <Route path="/" element={<LogIn />} /> */}
+					<Route path="/signup" element={<SignUpForm />} />
+					<Route path="/studentoverview" element={<StudentOverview />} />
+					<Route path="/student_projects" element={<ProjectDetails />} />
+					<Route path="/student_assignments" element={<AssignmentDetails />} />
+					<Route path="/project_feedback" element={<Feedback />} />
+					<Route path="/student_assessment" element={<AssessDetails />} />
+					<Route path="/student_card" element={<StudentCard />} />
+				</Routes>
+			</AuthContextProvider>
+		</div>
+	);
+};
 
-export default App
+export default App;
