9892161f0b6552c32a17e5cdb438484e1cf11e8e
JosephCarmeli <josephcarmeli@hotmail.com>
2023-07-17 19:31:22 -0500

    added email verification

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -5,6 +5,7 @@ import {
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
+  sendEmailVerification,
 } from "firebase/auth";
 import { auth, db } from "../../Firebase.js";
 import { doc, getDoc, setDoc } from "firebase/firestore";
@@ -16,6 +17,8 @@ export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
   const [role, setRole] = useState("");
   const navigate = useNavigate();
+  const [isUserNew, setIsUserNew] = useState(false);
+  const [hasJustVerified, setHasJustVerified] = useState(false);
 
   const getUserRole = async (uid) => {
     const docRef = doc(db, "users", uid);
@@ -33,12 +36,29 @@ export const AuthContextProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password).then(
       (response) => {
         const uid = response.user.uid;
+        setIsUserNew(true);
+        navigate("/");
+
+        const actionCodeSettings = {
+          url: "http://localhost:3000/login",
+        };
+
+        sendEmailVerification(response.user, actionCodeSettings)
+          .then(() => {
+            console.log("Verification email sent");
+            navigate("/");
+          })
+          .catch((error) => {
+            console.log("Error sending verification email", error);
+          });
+
         return setDoc(doc(db, "users", uid), { role });
       }
     );
   };
 
   const signIn = (email, password) => {
+    setHasJustVerified(false);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
@@ -49,15 +69,21 @@ export const AuthContextProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
       console.log(currentUser);
+      setIsUserNew(false);
       if (currentUser) {
         const docRef = doc(db, "users", currentUser.uid);
         const docSnap = await getDoc(docRef);
         const role = docSnap.exists() ? docSnap.data().role : null;
         setUser({ ...currentUser, role });
-        if (role === "instructor") {
-          navigate("/instructoroverview");
-        } else if (role === "student") {
-          navigate("/studentoverview");
+        if (currentUser.emailVerified && !hasJustVerified) {
+          if (role === "instructor") {
+            navigate("/instructoroverview");
+          } else if (role === "student") {
+            navigate("/studentoverview");
+          }
+        } else {
+          setHasJustVerified(true);
+          navigate("/");
         }
       } else {
         setUser(null);
@@ -68,7 +94,9 @@ export const AuthContextProvider = ({ children }) => {
     };
   }, [navigate]);
   return (
-    <UserContext.Provider value={{ createUser, user, logout, signIn, role }}>
+    <UserContext.Provider
+      value={{ createUser, user, logout, signIn, role, isUserNew }}
+    >
       {children}
     </UserContext.Provider>
   );
