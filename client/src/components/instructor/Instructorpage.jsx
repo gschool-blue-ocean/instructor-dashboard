794c732b36fa3df1c2f330661c38bed29c175fe2
John Kluse <j.kluse@gmail.com>
2023-07-18 10:33:47 -0600

    Update Instructorpage.jsx
    Added useEffect

--- client/src/components/instructor/Instructorpage.jsx
+++ client/src/components/instructor/Instructorpage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserAuth } from "../../context/authContext";
 import ProgressBar from "react-customizable-progressbar";
