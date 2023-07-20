78f747c391f6958d8856547d0cf3a589d2cc86a9
timgall <119544130+timgall@users.noreply.github.com>
2023-07-20 13:07:32 -0500

    updated buttons and linked data

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
 import {
-	AuthContextProvider,
-	UserAuth,
-	useRole,
+  AuthContextProvider,
+  UserAuth,
+  useRole,
 } from "../../context/authContext";
 import LogIn from "../LogIn/LogIn";
 import SignUpForm from "../Sign-up Page/SignUpForm";
@@ -20,122 +20,140 @@ import AddStudent from "../AddStudent";
 import Instructorpage from "../instructor/Instructorpage";
 
 const App = () => {
-	const [showSideBar, setShowSideBar] = useState(false);
-	const [hideHeader, setHideHeader] = useState(false);
-	const navigate = useNavigate();
-	const { user, isUserNew } = UserAuth();
-	const role = useRole();
-	const location = useLocation();
-	const [studentInfo, setStudentInfo] = useState("Instructor");
-	const [students, setStudents] = useState([]);
+  const [showSideBar, setShowSideBar] = useState(false);
+  const [hideHeader, setHideHeader] = useState(false);
+  const navigate = useNavigate();
+  const { user, isUserNew } = UserAuth();
+  const role = useRole();
+  const location = useLocation();
+  const [studentInfo, setStudentInfo] = useState("Instructor");
+  const [studentHeaderInfo, setStudentHeaderInfo] = useState("");
+  const [students, setStudents] = useState([]);
 
-	function updateStudentInfo(info) {
-		setStudentInfo(info);
-	}
+  function updateStudentInfo(info) {
+    setStudentInfo(info);
+  }
+  function updateStudentHeaderInfo(info) {
+    setStudentHeaderInfo(info);
+    console.log(info);
+  }
 
-	const createStudent = useCallback(
-		(newStudent) => {
-			setStudents([...students, newStudent]);
-		},
-		[students]
-	);
+  const createStudent = useCallback(
+    (newStudent) => {
+      setStudents([...students, newStudent]);
+    },
+    [students]
+  );
 
-	useEffect(() => {
-		if (showSideBar) {
-			document.body.classList.add("sidebar-open");
-		} else {
-			document.body.classList.remove("sidebar-open");
-		}
-		// Check if the current location corresponds to the LogIn or SignUpForm routes
-		const isLogInOrSignUp =
-			location.pathname === "/" || location.pathname === "/signup";
-		setHideHeader(isLogInOrSignUp);
-	}, [showSideBar, location]);
+  useEffect(() => {
+    if (showSideBar) {
+      document.body.classList.add("sidebar-open");
+    } else {
+      document.body.classList.remove("sidebar-open");
+    }
+    // Check if the current location corresponds to the LogIn or SignUpForm routes
+    const isLogInOrSignUp =
+      location.pathname === "/" || location.pathname === "/signup";
+    setHideHeader(isLogInOrSignUp);
+  }, [showSideBar, location]);
 
-	const containerStyle = {
-		marginLeft: showSideBar ? "240px" : "0",
-		transition: "margin-left 0.3s ease",
-	};
+  const containerStyle = {
+    marginLeft: showSideBar ? "240px" : "0",
+    transition: "margin-left 0.3s ease",
+  };
 
-	if (user === undefined) {
-		// Still determining if the user is logged in.
-		return <div>Loading...</div>;
-	}
-	if (user === null || isUserNew || (user && !user.emailVerified)) {
-		return (
-			<Routes>
-				<Route
-					path="/"
-					element={<LogIn updateStudentId={updateStudentInfo} />}
-				/>
-				<Route path="/signup" element={<SignUpForm />} />
-			</Routes>
-		);
-	}
-	if (role === "student") {
-		return (
-			<div>
-				<h1 className="text-center text-3xl font-bold"></h1>
-				<div style={containerStyle}>
-					<Header showSideBar={showSideBar} setShowSideBar={setShowSideBar} />
-					<Routes>
-						<Route
-							path="/studentoverview"
-							element={
-								<>
-									<StudentOverview
-										studentInfo={studentInfo}
-										updateStudentInfo={updateStudentInfo}
-									/>
-								</>
-							}
-						/>
-						<Route
-							path="/"
-							element={
-								<>
-									<StudentOverview
-										studentInfo={studentInfo}
-										updateStudentInfo={updateStudentInfo}
-									/>
-								</>
-							}
-						/>
-					</Routes>
-				</div>{" "}
-				<div style={containerStyle}></div>
-			</div>
-		);
-	}
-	if (role === "instructor") {
-		return (
-			<div>
-				<h1 className="text-center text-3xl font-bold"></h1>
-				<div style={containerStyle}>
-					<Header showSideBar={showSideBar} setShowSideBar={setShowSideBar} />
-					<Routes>
-						<Route
-							path="/studentoverview/:studentId"
-							element={
-								<>
-									<StudentOverview
-										studentInfo={studentInfo}
-										updateStudentInfo={updateStudentInfo}
-									/>
-								</>
-							}
-						/>
-						<Route
-							path="/addstudent"
-							element={<AddStudent createStudent={createStudent} role={role} />}
-						/>
-						<Route path="/" element={<Instructorpage />} />
-						<Route path="/instructoroverview" element={<Instructorpage />} />
-					</Routes>
-				</div>{" "}
-				<div style={containerStyle}></div>
-			</div>
-		);
-	}
+  if (user === undefined) {
+    // Still determining if the user is logged in.
+    return <div>Loading...</div>;
+  }
+  if (user === null || isUserNew || (user && !user.emailVerified)) {
+    return (
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <LogIn
+              updateStudentId={updateStudentInfo}
+              updateStudentHeader={updateStudentHeaderInfo}
+            />
+          }
+        />
+        <Route path="/signup" element={<SignUpForm />} />
+      </Routes>
+    );
+  }
+  if (role === "student") {
+    return (
+      <div>
+        <h1 className="text-center text-3xl font-bold"></h1>
+        <div style={containerStyle}>
+          <Header
+            showSideBar={showSideBar}
+            setShowSideBar={setShowSideBar}
+            studentHeaderInfo={studentHeaderInfo}
+          />
+          <Routes>
+            <Route
+              path="/studentoverview"
+              element={
+                <>
+                  <StudentOverview
+                    studentInfo={studentInfo}
+                    updateStudentInfo={updateStudentInfo}
+                  />
+                </>
+              }
+            />
+            <Route
+              path="/"
+              element={
+                <>
+                  <StudentOverview
+                    studentInfo={studentInfo}
+                    updateStudentInfo={updateStudentInfo}
+                  />
+                </>
+              }
+            />
+          </Routes>
+        </div>{" "}
+        <div style={containerStyle}></div>
+      </div>
+    );
+  }
+  if (role === "instructor") {
+    return (
+      <div>
+        <h1 className="text-center text-3xl font-bold"></h1>
+        <div style={containerStyle}>
+          <Header
+            showSideBar={showSideBar}
+            setShowSideBar={setShowSideBar}
+            studentHeaderInfo={studentHeaderInfo}
+          />
+          <Routes>
+            <Route
+              path="/studentoverview/:studentId"
+              element={
+                <>
+                  <StudentOverview
+                    studentInfo={studentInfo}
+                    updateStudentInfo={updateStudentInfo}
+                  />
+                </>
+              }
+            />
+            <Route
+              path="/addstudent"
+              element={<AddStudent createStudent={createStudent} role={role} />}
+            />
+            <Route path="/" element={<Instructorpage />} />
+            <Route path="/instructoroverview" element={<Instructorpage />} />
+          </Routes>
+        </div>{" "}
+        <div style={containerStyle}></div>
+      </div>
+    );
+  }
 };
 export default App;
--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -1,12 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
 import { createContext } from "react";
 import {
-	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword,
-	signOut,
-	onAuthStateChanged,
-	sendEmailVerification,
-	sendPasswordResetEmail,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  sendEmailVerification,
+  sendPasswordResetEmail,
 } from "firebase/auth";
 import { auth, db } from "../../Firebase.js";
 import { doc, getDoc, setDoc } from "firebase/firestore";
@@ -15,112 +15,96 @@ import { useNavigate } from "react-router-dom";
 const UserContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-	const [user, setUser] = useState({});
-	const [role, setRole] = useState("");
-	const navigate = useNavigate();
-	const [isUserNew, setIsUserNew] = useState(false);
-	const [hasJustVerified, setHasJustVerified] = useState(false);
+  const [user, setUser] = useState({});
+  const [role, setRole] = useState("");
+  const navigate = useNavigate();
+  const [isUserNew, setIsUserNew] = useState(false);
+  const [hasJustVerified, setHasJustVerified] = useState(false);
 
-	const getUserRole = async (uid) => {
-		const docRef = doc(db, "users", uid);
-		const docSnap = await getDoc(docRef);
+  const createUser = (email, password, role = "student") => {
+    return createUserWithEmailAndPassword(auth, email, password).then(
+      (response) => {
+        const uid = response.user.uid;
+        setIsUserNew(true);
+        navigate("/");
 
-		if (docSnap.exists()) {
-			return docSnap.data().role;
-		} else {
-			console.error("No such document!");
-			return null;
-		}
-	};
+        const actionCodeSettings = {
+          url: "http://localhost:3000/login",
+        };
 
-	const createUser = (email, password, role = "student") => {
-		return createUserWithEmailAndPassword(auth, email, password).then(
-			(response) => {
-				const uid = response.user.uid;
-				setIsUserNew(true);
-				navigate("/");
+        sendEmailVerification(response.user, actionCodeSettings)
+          .then(() => {
+            console.log("Verification email sent");
+            navigate("/");
+          })
+          .catch((error) => {
+            console.log("Error sending verification email", error);
+          });
 
-				const actionCodeSettings = {
-					url: "http://localhost:3000/login",
-				};
+        return setDoc(doc(db, "users", uid), { role });
+      }
+    );
+  };
 
-				sendEmailVerification(response.user, actionCodeSettings)
-					.then(() => {
-						console.log("Verification email sent");
-						navigate("/");
-					})
-					.catch((error) => {
-						console.log("Error sending verification email", error);
-					});
+  const signIn = (email, password) => {
+    setHasJustVerified(false);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-				return setDoc(doc(db, "users", uid), { role });
-			}
-		);
-	};
+  const logout = () => {
+    return signOut(auth);
+  };
 
-	const signIn = (email, password) => {
-		setHasJustVerified(false);
-		return signInWithEmailAndPassword(auth, email, password);
-	};
+  const sendResetPasswordEmail = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
 
-	const logout = () => {
-		return signOut(auth);
-	};
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      setIsUserNew(false);
+      if (currentUser) {
+        const docRef = doc(db, "users", currentUser.uid);
+        const docSnap = await getDoc(docRef);
+        const role = docSnap.exists() ? docSnap.data().role : null;
 
-	const sendResetPasswordEmail = (email) => {
-		return sendPasswordResetEmail(auth, email);
-	};
+        setUser({ ...currentUser, role });
 
-	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
-			console.log(currentUser);
-			setIsUserNew(false);
-			if (currentUser) {
-				const docRef = doc(db, "users", currentUser.uid);
-				const docSnap = await getDoc(docRef);
-				const role = docSnap.exists() ? docSnap.data().role : null;
-				setUser({ ...currentUser, role });
-				//The commented code below will cause redirects
-				if (currentUser.emailVerified && !hasJustVerified) {
-					// if (role === "instructor") {
-					//   navigate("/instructoroverview");
-					// } else if (role === "student") {
-					//   navigate("/studentoverview");
-					// }
-				} else {
-					setHasJustVerified(true);
-					navigate("/");
-				}
-			} else {
-				setUser(null);
-			}
-		});
-		return () => {
-			unsubscribe();
-		};
-	}, [navigate]);
-	return (
-		<UserContext.Provider
-			value={{
-				createUser,
-				user,
-				logout,
-				signIn,
-				role,
-				isUserNew,
-				sendResetPasswordEmail,
-			}}
-		>
-			{children}
-		</UserContext.Provider>
-	);
+        if (currentUser.emailVerified && !hasJustVerified) {
+          // You can add specific logic here when the user is logged in and email is verified
+        } else {
+          // You can add specific logic here when the user is logged in but email is not verified or any other scenarios
+        }
+      } else {
+        setUser(null);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [navigate]);
+
+  return (
+    <UserContext.Provider
+      value={{
+        createUser,
+        user,
+        logout,
+        signIn,
+        role,
+        isUserNew,
+        sendResetPasswordEmail,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export const UserAuth = () => {
-	return useContext(UserContext);
+  return useContext(UserContext);
 };
 
 export const useRole = () => {
-	const { user } = useContext(UserContext);
-	return user?.role;
+  const { user } = useContext(UserContext);
+  return user?.role;
 };
